309d5f7d6a010cf029bbdd3716b98f63f4ade011
tayeb83 <tayeb.merabti@gmail.com>
2012-09-26 07:36:05 +0200

    first commit

---
+++ app/controllers/pages_controller.rb
@@ -0,0 +1,16 @@
+class PagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def project
+  end
+
+  def service
+  end
+
+  def blog
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SiteDaade::Application.config.secret_token = 'fe8e00ec99b871e5ac629e9023d3645c05e2eba56509807c98caca581ad8060e6543b360fe65444fba006c57e391eea1e8669eb5555a196db10a36ac7c6304eb'
